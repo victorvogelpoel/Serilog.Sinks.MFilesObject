2c5d255e22fe62392fdb251564a2be1972718389
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-05-27 15:25:15 +0200

    Add header license

--- src/DemoVaultApplication/Properties/AssemblyInfo.cs
+++ src/DemoVaultApplication/Properties/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+// AssemblyInfo.cs
+// 14-5-2021
+// Copyright 2021 Dramatic Development - Victor Vogelpoel
+// If this works, it was written by Victor Vogelpoel (victor@victorvogelpoel.nl).
+// If it doesn't, I don't know who wrote it.
+//
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
--- src/sandbox/Program.cs
+++ src/sandbox/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Program.cs
-// 5 2021
+// 14-5-2021
 // Copyright 2021 Dramatic Development - Victor Vogelpoel
 // If this works, it was written by Victor Vogelpoel (victor@victorvogelpoel.nl).
 // If it doesn't, I don't know who wrote it.

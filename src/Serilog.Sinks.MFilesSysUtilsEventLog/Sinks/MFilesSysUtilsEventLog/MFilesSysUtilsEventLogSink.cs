2c30beac2083b14030a87e60d003aba1d65f8a3d
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-05-27 14:58:02 +0200

    Remove MFilesSysUtilsEventLogSinkExtensions
    This class is alread present at root level

--- src/Serilog.Sinks.MFilesSysUtilsEventLog/Sinks/MFilesSysUtilsEventLog/MFilesSysUtilsEventLogSink.cs
+++ src/Serilog.Sinks.MFilesSysUtilsEventLog/Sinks/MFilesSysUtilsEventLog/MFilesSysUtilsEventLogSink.cs
@@ -48,13 +48,4 @@ namespace Serilog.Sinks.MFilesSysUtilsEventLog
             SysUtils.ReportToEventLog(message, entryType);
         }
     }
-
-
-    public static class MFilesSysUtilsEventLogSinkExtensions
-    {
-        public static LoggerConfiguration MFilesSysUtilsEventLogSink(this LoggerSinkConfiguration loggerSinkConfiguration, LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum, IFormatProvider formatProvider = null, LoggingLevelSwitch levelSwitch = null)
-        {
-            return loggerSinkConfiguration.Sink(new MFilesSysUtilsEventLogSink(formatProvider), restrictedToMinimumLevel, levelSwitch);
-        }
-    }
 }

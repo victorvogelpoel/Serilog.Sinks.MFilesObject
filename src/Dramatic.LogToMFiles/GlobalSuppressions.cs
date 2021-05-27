e4e924603d8b6a46cab392bda0ebd63fd7f8c86a
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-05-27 16:02:58 +0200

    Add global suppressions

---
+++ src/Dramatic.LogToMFiles/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:Dramatic.LogToMFiles.MFilesLogMessageRepository.IsObjectCheckedOut(MFilesAPI.ObjID,System.Int32)~System.Boolean")]
+[assembly: SuppressMessage("Critical Code Smell", "S927:Parameter names should match base declaration and other partial definitions", Justification = "<Pending>", Scope = "member", Target = "~M:Dramatic.LogToMFiles.LogObjectCreatedComparer.Compare(MFilesAPI.ObjectVersion,MFilesAPI.ObjectVersion)~System.Int32")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:Dramatic.LogToMFiles.MFilesLogMessageRepository.WriteLogMessage(System.String)")]
+[assembly: SuppressMessage("Info Code Smell", "S1135:Track uses of \"TODO\" tags", Justification = "<Pending>", Scope = "type", Target = "~T:Dramatic.LogToMFiles.MFilesObjectLogSinkVaultStructure")]

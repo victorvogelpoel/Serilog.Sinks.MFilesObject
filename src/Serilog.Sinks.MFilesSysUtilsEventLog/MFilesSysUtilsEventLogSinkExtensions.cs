f7aaafec297d07a1e2338c1804619dd5eb4d66a4
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-05-27 14:57:30 +0200

    Add license header

--- src/Serilog.Sinks.MFilesSysUtilsEventLog/MFilesSysUtilsEventLogSinkExtensions.cs
+++ src/Serilog.Sinks.MFilesSysUtilsEventLog/MFilesSysUtilsEventLogSinkExtensions.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// MFilesSysUtilsEventLogSinkExtensions.cs
+// 18-5-2021
+// Copyright 2021 Dramatic Development - Victor Vogelpoel
+// If this works, it was written by Victor Vogelpoel (victor@victorvogelpoel.nl).
+// If it doesn't, I don't know who wrote it.
+//
+using System;
 using Serilog.Core;
 using Serilog.Events;
 using Serilog.Configuration;

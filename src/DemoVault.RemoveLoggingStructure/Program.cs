c6f5ca47ee46cde09854f45360ecf6200832c1e6
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-07-18 15:51:00 +0200

    Add DemoVault.AddLoggingStructure and DemoVault.RemoveLoggingStructure console programs
    These two ensure or remove logging structure from the vault using a console program (instead of doing that by hand)

---
+++ src/DemoVault.RemoveLoggingStructure/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Dramatic.LogToMFiles;
+
+namespace DemoVault.RemoveLoggingStructure
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string vaultname        = "Serilog.Sinks.MFilesObject";
+
+                Console.WriteLine($"Connecting to vault \"{vaultname}\" using the logged on windows user...");
+                var serverApp           = new MFilesAPI.MFilesServerApplication();
+                serverApp.Connect(MFilesAPI.MFAuthType.MFAuthTypeLoggedOnWindowsUser);
+                var vaultOnServer       = serverApp.GetOnlineVaults().GetVaultByName(vaultname); // The "Serilog.Sinks.MFilesObject" demo vault that mysteriously bears the same name as the logging solution
+                var vault               = serverApp.LogInAsUserToVault(vaultOnServer.GUID);  // "{D449E438-89EE-42BB-9769-B862E9B1B140}"
+
+                Console.WriteLine("Connected and logged in to vault :-)");
+
+                // Define vault structure for logging if it isn't there: OT "Log", CL "Log" and PD "LogMessage" and aliases to find them back.
+                var structureConfig = new MFilesLoggingVaultStructureConfiguration
+                {
+                    LogObjectTypeNameSingular   = "Log",
+                    LogObjectTypeNamePlural     = "Logs",
+                    LogMessagePropDefName       = "LogMessage",
+
+                    LogObjectTypeAlias          = "OT.Serilog.MFilesObjectLogSink.Log",
+                    LogClassAlias               = "CL.Serilog.MFilesObjectLogSink.Log",
+                    LogMessagePropDefAlias      = "PD.Serilog.MFilesObjectLogSink.LogMessage",
+
+                    LogFileClassName            = "LogFile",
+                    LogFileClassAlias           = "CL.Serilog.MFilesObjectLogSink.LogFile"
+                };
+
+                Console.WriteLine("Removing Log and LogFile objects from the vault and removing logging vault structure...");
+
+                // Ensure that the structure for Logging object and class is present in the vault (needs FULL ADMIN permissions on vault)
+                vault.RemoveLogObjectsAndLoggingVaultStructure(structureConfig);
+
+                Console.WriteLine("Completed removing Log and LogFile objects and removing logging structure from the vault...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToDetailedString());
+            }
+        }
+    }
+
+
+
+
+    // source: https://gist.github.com/RehanSaeed/c256828acc04d685d024
+    public static class ExceptionExtensions
+    {
+        public static string ToDetailedString(this Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            return ToDetailedString(exception, ExceptionOptions.Default);
+        }
+
+        public static string ToDetailedString(this Exception exception, ExceptionOptions options)
+        {
+            var stringBuilder = new StringBuilder();
+
+            AppendValue(stringBuilder, "Type", exception.GetType().FullName, options);
+
+            foreach (PropertyInfo property in exception
+                .GetType()
+                .GetProperties()
+                .OrderByDescending(x => string.Equals(x.Name, nameof(exception.Message), StringComparison.Ordinal))
+                .ThenByDescending(x => string.Equals(x.Name, nameof(exception.Source), StringComparison.Ordinal))
+                .ThenBy(x => string.Equals(x.Name, nameof(exception.InnerException), StringComparison.Ordinal))
+                .ThenBy(x => string.Equals(x.Name, nameof(AggregateException.InnerExceptions), StringComparison.Ordinal)))
+            {
+                var value = property.GetValue(exception, null);
+                if (value == null && options.OmitNullProperties)
+                {
+                    if (options.OmitNullProperties)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        value = string.Empty;
+                    }
+                }
+
+                AppendValue(stringBuilder, property.Name, value, options);
+            }
+
+            return stringBuilder.ToString().TrimEnd('\r', '\n');
+        }
+
+        private static void AppendCollection(
+            StringBuilder stringBuilder,
+            string propertyName,
+            IEnumerable collection,
+            ExceptionOptions options)
+        {
+            stringBuilder.AppendLine($"{options.Indent}{propertyName} =");
+
+            var innerOptions = new ExceptionOptions(options, options.CurrentIndentLevel + 1);
+
+            var i = 0;
+            foreach (var item in collection)
+            {
+                var innerPropertyName = $"[{i}]";
+
+                if (item is Exception)
+                {
+                    var innerException = (Exception)item;
+                    AppendException(
+                        stringBuilder,
+                        innerPropertyName,
+                        innerException,
+                        innerOptions);
+                }
+                else
+                {
+                    AppendValue(
+                        stringBuilder,
+                        innerPropertyName,
+                        item,
+                        innerOptions);
+                }
+
+                ++i;
+            }
+        }
+
+        private static void AppendException(
+            StringBuilder stringBuilder,
+            string propertyName,
+            Exception exception,
+            ExceptionOptions options)
+        {
+            var innerExceptionString = ToDetailedString(
+                exception,
+                new ExceptionOptions(options, options.CurrentIndentLevel + 1));
+
+            stringBuilder.AppendLine($"{options.Indent}{propertyName} =");
+            stringBuilder.AppendLine(innerExceptionString);
+        }
+
+        private static string IndentString(string value, ExceptionOptions options)
+        {
+            return value.Replace(Environment.NewLine, Environment.NewLine + options.Indent);
+        }
+
+        private static void AppendValue(
+            StringBuilder stringBuilder,
+            string propertyName,
+            object value,
+            ExceptionOptions options)
+        {
+            if (value is DictionaryEntry)
+            {
+                DictionaryEntry dictionaryEntry = (DictionaryEntry)value;
+                stringBuilder.AppendLine($"{options.Indent}{propertyName} = {dictionaryEntry.Key} : {dictionaryEntry.Value}");
+            }
+            else if (value is Exception)
+            {
+                var innerException = (Exception)value;
+                AppendException(
+                    stringBuilder,
+                    propertyName,
+                    innerException,
+                    options);
+            }
+            else if (value is IEnumerable && !(value is string))
+            {
+                var collection = (IEnumerable)value;
+                if (collection.GetEnumerator().MoveNext())
+                {
+                    AppendCollection(
+                        stringBuilder,
+                        propertyName,
+                        collection,
+                        options);
+                }
+            }
+            else
+            {
+                stringBuilder.AppendLine($"{options.Indent}{propertyName} = {value}");
+            }
+        }
+    }
+
+    public struct ExceptionOptions
+    {
+        public static readonly ExceptionOptions Default = new ExceptionOptions()
+        {
+            CurrentIndentLevel = 0,
+            IndentSpaces = 4,
+            OmitNullProperties = true
+        };
+
+        internal ExceptionOptions(ExceptionOptions options, int currentIndent)
+        {
+            this.CurrentIndentLevel = currentIndent;
+            this.IndentSpaces = options.IndentSpaces;
+            this.OmitNullProperties = options.OmitNullProperties;
+        }
+
+        internal string Indent { get { return new string(' ', this.IndentSpaces * this.CurrentIndentLevel); } }
+
+        internal int CurrentIndentLevel { get; set; }
+
+        public int IndentSpaces { get; set; }
+
+        public bool OmitNullProperties { get; set; }
+    }
+
+}
